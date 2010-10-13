a3189d0c614d1c36ff33cbc9fed4d64310cc43f1
JGrubb <therealjohnnygrubb@gmail.com>
2010-10-13 07:36:18 -0400

    initial commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<div id="header">
+	
+	<div id="block-top"></div>
+	
+	<div id="personal-info" class="fixed">
+		
+		<h1 id="name">John Grubb</h1>
+		<h2 id="what-you-do">Job title / Expertise</h2>
+		
+	</div><!-- end #personal-info -->
+			
+	<ul id="nav" class="fixed">
+		<li><a href="index.html">Resume</a></li>
+		<li>/</li>
+		<li><a href="portfolio.html">Portfolio</a></li>
+		<li>/</li>
+		<li><a href="contact.html" class="active">Contact</a></li>
+	</ul><!-- end #nav -->
+
+	<div class="divider"></div>
+	
+</div><!-- end #header -->
